9a01f170eead90265778b33b42ccd07fbdf37a41
drupchen <hhdrupchen@gmail.com>
2018-12-30 20:25:39 +0100

    put everything in a single line

---
+++ scripts/add_spaces.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+for f in Path('.').glob('*.txt'):
+    print(f.name)
+    content = f.read_text()
+    idx = content[:content.find(':')].rfind('\n') -1
+    no_notes, notes = content[:idx], content[idx:]
+    no_notes = no_notes.replace('\n', ' ')
+    content = no_notes + notes
+    f.write_text(content)
